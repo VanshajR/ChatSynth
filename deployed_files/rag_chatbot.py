93357601e5ce13db1b0db97ca66cf8da8d051fff
Vanshaj Raghuvanshi <vanshajraghuvanshi@gmail.com>
2025-11-03 11:10:09 +0530

    Updated import paths for document and retrieval chains

--- deployed_files/rag_chatbot.py
+++ deployed_files/rag_chatbot.py
@@ -2,12 +2,13 @@ import os
 import json
 import streamlit as st
 from langchain_groq import ChatGroq
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.combine_documents.chain import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from chatsynth_vanshajr.retriever import ChatSynthRetriever
+
 
 def chat_assistant():
     # Load secrets
